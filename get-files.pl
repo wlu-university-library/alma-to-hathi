8b452794f71d6b35b333d1c13e7de986cb368d57
mickelj@wlu.edu <mickelj@wlu.edu>
2020-02-18 16:44:30 -0500

    added download to local server with secrets yaml file

---
+++ get-files.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use YAML::XS 'LoadFile';
+my $secrets = LoadFile('secrets.yaml');
+
+my $u = $secrets->{username};
+my $p = $secrets->{password};
+my $s = $secrets->{server};
+my $h = $secrets->{path};
+
+system ("sshpass -p $p scp -r $u\@$s\:$h /opt/hathi/alma");
